28a833c3f61b3b4449e0febe6a5c2db0e1b34ee1
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-02-09 20:51:48 +0530

    Replace database connection module with config
    Updated database connection details.

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-module.exports = {
-  connect: () => {
-    console.log("User Service DB connected");
-  }
-};
+DB_HOST=database-1.co3kus82asfr.us-east-1.rds.amazonaws.com
+DB_USER=admin
+DB_PASSWORD=uuwawEtf7c2XuakgDMuA
+DB_NAME=database-1
+DB_PORT=3306
