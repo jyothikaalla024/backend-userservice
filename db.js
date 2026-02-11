ed616572801bb3ab90d0ecf087a1d5416408cc04
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-02-11 20:29:39 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,28 @@
-DB_HOST=database-1.co3kus82asfr.us-east-1.rds.amazonaws.com
-DB_USER=admin
-DB_PASSWORD=GPX86Gif3xasvFVrUdCq
-DB_NAME=database-1
-DB_PORT=3306
+const mysql = require("mysql2/promise");
+
+// Create connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000
+});
+
+// Test DB connection
+const connect = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("User Service DB connected");
+    connection.release();
+  } catch (err) {
+    console.error("User Service DB connection failed:", err.message);
+    process.exit(1); // Important in ECS
+  }
+};
+
+module.expor
